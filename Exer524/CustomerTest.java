8d9d5374b3f3e7f818de3625f1471a336345efc3
Dragon-yjl <2286920873@qq.com>
2021-05-24 11:02:10 +0800

    构造器练习

---
+++ Exer524/CustomerTest.java
@@ -0,0 +1,21 @@
+package Exer1;
+
+public class CustomerTest {
+    public static void main(String[] args) {
+
+        Customer cust = new Customer("Jane","Smith");
+
+        Account acct = new Account(1000,2000,0.0123);
+
+        cust.setAccount(acct);
+
+        cust.getAccount().deposit(100);
+        cust.getAccount().withdraw(960);
+        cust.getAccount().withdraw(2000);
+
+        System.out.println("Customer [" + cust.getLastName()+","+cust.getFirstName()+"] has a account : id is "+
+                cust.getAccount().getId()+",annualInterrstRate is "+cust.getAccount().getAnnualInterestRate()
+                *100+"%,balance is " + cust.getAccount().getBalance());
+
+    }
+}
