5e92299ab81de6abe87e3cfffb6f5a35dddc5b18
Dragon-yjl <2286920873@qq.com>
2021-07-31 23:18:44 +0800

    牛客网计算球体体积

---
+++ nowcoder0731/Main.java
@@ -0,0 +1,13 @@
+package nowcoder0731;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        double r = s.nextDouble();
+        double pai = 3.1415926;
+        System.out.printf("%.3f",(4.0/3)*pai*r*r*r);
+    }
+}
