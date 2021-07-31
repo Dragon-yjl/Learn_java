97948e105aaa205b07b5bd92dce95b75586d6102
Dragon-yjl <2286920873@qq.com>
2021-07-31 23:25:30 +0800

    牛客网字母大小写转换

---
+++ nowCoder07_31/Main.java
@@ -0,0 +1,14 @@
+package nowCoder07_31;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        while (s.hasNext()) {
+            System.out.println(s.nextLine().toLowerCase());
+        }
+    }
+}
