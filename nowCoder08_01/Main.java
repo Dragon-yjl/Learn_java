c6ac48bf8b100cb6ef1b7aa94bf19f51a1cd1ea3
Dragon-yjl <2286920873@qq.com>
2021-08-01 23:17:03 +0800

    牛客网 字符转ASCII码

---
+++ nowCoder08_01/Main.java
@@ -0,0 +1,13 @@
+package nowCoder08_01;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        char c = str.charAt(0);
+
+        System.out.println((int)c);
+    }
+}
