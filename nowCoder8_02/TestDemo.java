50d375fd3f92afb18338bdf857e806fe1c738dab
Dragon-yjl <2286920873@qq.com>
2021-08-02 22:59:58 +0800

    牛客网 输入输出格式

---
+++ nowCoder8_02/TestDemo.java
@@ -0,0 +1,10 @@
+package nowCoder8_02;
+
+public class TestDemo {
+    public static void main(String[] args) {
+
+        System.out.println("Name    Age    Gender");
+        System.out.println("---------------------");
+        System.out.println("Jack    18      man");
+    }
+}
