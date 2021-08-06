f168ad35531a29b8638b68a5cfd9eccbb0bd495b
Dragon-yjl <2286920873@qq.com>
2021-08-06 22:29:52 +0800

    牛客网 BC41

---
+++ nowCoder0806/Main.java
@@ -0,0 +1,14 @@
+package nowCoder0806;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int iq = s.nextInt();
+        if(iq >= 140) {
+            System.out.println("Genius");
+        }
+    }
+}
