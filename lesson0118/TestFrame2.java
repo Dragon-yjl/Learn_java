265ba6e78bdff5bb35be8a7597d9dae7ae27c157
Dragon-yjl <2286920873@qq.com>
2022-01-18 22:28:17 +0800

    初识GUI编程

---
+++ lesson0118/TestFrame2.java
@@ -0,0 +1,20 @@
+package com.yjl.lesson0118;
+
+import java.awt.*;
+
+public class TestFrame2 {
+    public static void main(String[] args) {
+
+        // 展示多个窗口
+    }
+}
+
+
+class MyFrame extends Frame{
+
+    static int id = 0; // 可能存在多个窗口，需要一个计数器来表示
+
+    public MyFrame(int x,int y,int w,int h,Color color) {
+
+    }
+}
